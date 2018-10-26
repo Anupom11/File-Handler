550c3914a9e2f7da589e52d3c9afad25d3bda028
Anupom Chakrabarty <30777289+Anupom11@users.noreply.github.com>
2018-10-26 23:53:49 +0530

    Update delete_matter_details.php

--- filehandling.org/delete_matter_details.php
+++ filehandling.org/delete_matter_details.php
@@ -5,11 +5,19 @@
 
 <html>
 	<head>
-		<title>Delete Matter Details</title>
+		<title> Delete File Index </title>
 	</head>
 	<body>
 	
 	<?php	
+	
+		//******************************************************************************
+		require_once 'C:\wamp\www\filehandling.org\db_connections\dbConfig.php';
+		require_once 'C:\wamp\www\filehandling.org\db_connections\dbAdapter.php';	
+	
+		$dbConnect = new fileHandlerDB($pdo);	// creating db class object
+	//*******************************************************************************
+	
 		if(isset($_SESSION['user_name']) && isset($_SESSION['sID']))
 		{
 			$user_name = $_SESSION['user_name'];
@@ -26,24 +34,33 @@
 		// checking for the session id
 		if($sessionID == "login_success")
 		{
-			if(isset($_GET['number']))
+			if(isset($_GET['fileIndex']))
 			{
-				$slNumber = $_GET['number'];
-								
+				$fileIndexVal = $_GET['fileIndex'];
+				
 				// ***********************now delete the following file index value**********************************************
 				// create connection with the database
 				// connecting to the database
-				$conn 	= mysql_connect('127.0.0.1', 'root', 'admin') or die("Can not connect with the server.");
-				$db		= mysql_select_db('office_file_handling', $conn) or die("Can not select the database.");
+				//$conn 	= mysql_connect('127.0.0.1', 'root', 'admin') or die("Can not connect with the server.");
+				//$db		= mysql_select_db('office_file_handling', $conn) or die("Can not select the database.");
 				
-				$delete_query = "delete from document_details where sl_no='$slNumber'";
-				$deleteResult = mysql_query($delete_query) or die(mysql_error());
+				//$delete_query = "delete from document_details where file_index='$fileIndexVal'";
+				//$deleteResult = mysql_query($delete_query) or die(mysql_error());
+				
+				$deleteResult = $dbConnect->deleteFileIndex($fileIndexVal);
 				
 				// if successfully deleted then redirect to the dashboard page else show error message 
-				if($deleteResult)
+				if($deleteResult>0)
 				{
 					echo "Deleted successfully!";
 					header('location:dashboard.php');
+				}
+				else if(deleteResult == 0)
+				{
+					echo "Delete operation failed!"
+				?>
+					<h3 align="left">Please <a href="dashboard.php> go back </a></h3>
+				<?php
 				}
 			}
 			else
