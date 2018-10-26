dbe9c601a2b04539a4696da401465fab15c26c52
Anupom Chakrabarty <30777289+Anupom11@users.noreply.github.com>
2018-10-26 23:54:57 +0530

    Update delete_user_account.php

--- filehandling.org/delete_user_account.php
+++ filehandling.org/delete_user_account.php
@@ -7,6 +7,7 @@
 	
 	<body>
 		<?php
+				
 		// checking for session validation
 		if(isset($_SESSION['user_name']) && isset($_SESSION['sID']))
 		{
@@ -25,6 +26,13 @@
 		{
 			function delete_user_account()
 			{
+				//******************************************************************************
+				require_once 'C:\wamp\www\filehandling.org\db_connections\dbConfig.php';
+				require_once 'C:\wamp\www\filehandling.org\db_connections\dbAdapter.php';	
+		
+				$dbConnect = new fileHandlerDB($pdo);	// creating db class object
+				//*******************************************************************************
+				
 				// doing deletation operation
 
 				// $response = array();
@@ -38,16 +46,23 @@
 				// echo $user_name;
 				
 				// do the connection with the server and with the database.
-				$conn 	= mysql_connect('127.0.0.1', 'root', 'admin') or die("Can not connect with the server.");
-				$db		= mysql_select_db('office_file_handling', $conn) or die("Can not select the database.");
+				//$conn 	= mysql_connect('127.0.0.1', 'root', 'admin') or die("Can not connect with the server.");
+				//$db		= mysql_select_db('office_file_handling', $conn) or die("Can not select the database.");
 					
-				$query 	= "delete from user_account where user_name='$user_name' and user_id='$uid' and password='$pwd'";
-				$result	= mysql_query($query);
-
-				if($result){
+				//$query 	= "delete from user_account where user_name='$user_name' and user_id='$uid' and password='$pwd'";
+				//$result	= mysql_query($query);
+				
+				$result = $dbConnect->deleteUserAccount($user_name, $uid, $pwd);
+				
+				if($result>0) {
 					//echo "Successfully deleted";
 					return true;
-				}else{
+				}
+				else if($result == 0) {
+					echo "Delete operation failed!";
+					return false;
+				}
+				else {
 					//echo "Something went wrong.";
 					return false;
 				}
